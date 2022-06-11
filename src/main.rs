fc8909f59eed96b0b1d78ff71ce7f5583c822b3e
Chad Collins <chadrcollins91@gmail.com>
2022-06-11 10:23:36 -0400

    Made trait for handling widget functionality

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,134 @@
 use std::io;
+use std::io::Stdout;
 
-use crossterm::event::{read, DisableMouseCapture, EnableMouseCapture, Event, KeyCode};
+use crossterm::event::{read, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyModifiers, KeyEvent};
 use crossterm::execute;
 use crossterm::style::Print;
 use crossterm::terminal::{
     disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
 };
 use tui::backend::CrosstermBackend;
-use tui::layout::{Alignment, Constraint, Direction, Layout};
+use tui::layout::{Alignment, Constraint, Direction, Layout, Rect};
 use tui::style::{Color, Style};
 use tui::text::Text;
-use tui::widgets::{Block, Borders, Paragraph, Wrap};
-use tui::Terminal;
+use tui::widgets::{Block, Borders, Paragraph, Widget, Wrap};
+use tui::{Frame, Terminal};
+use tui::buffer::Buffer;
+
+struct AppState {
+    active_panel: usize,
+}
+
+trait Panel {
+    fn make_widget(&self, frame: &mut EditorFrame, rect: Rect);
+    fn get_cursor(&self, rect: &Rect) -> (u16, u16);
+    fn receive_key(&mut self, event: KeyEvent) -> bool;
+    fn set_active(&mut self);
+}
+
+trait ChordReceiver {
+    fn receive(
+        &self,
+        current_panel: &mut Box<dyn Panel>,
+        app_state: &mut AppState,
+        code: KeyCode,
+    ) -> KeyInputState;
+}
+
+enum KeyInputState {
+    Normal,
+}
+
+struct TextEditPanel {
+    min_x: u16,
+    min_y: u16,
+    cursor_x: u16,
+    cursor_y: u16,
+    text: String,
+}
+
+impl TextEditPanel {
+    fn new() -> Self {
+        TextEditPanel {
+            cursor_x: 1,
+            cursor_y: 1,
+            min_x: 1,
+            min_y: 1,
+            text: String::new(),
+        }
+    }
+}
+
+impl Panel for TextEditPanel {
+    fn make_widget(&self, frame: &mut EditorFrame, rect: Rect) {
+        let para_text = Text::from(self.text.clone());
+        let para_block = Block::default().title("Block").borders(Borders::ALL);
+        let para = Paragraph::new(para_text)
+            .block(para_block)
+            .style(Style::default().fg(Color::White).bg(Color::Black))
+            .alignment(Alignment::Left)
+            .wrap(Wrap { trim: true });
+
+        frame.render_widget(para, rect);
+    }
+
+    fn get_cursor(&self, rect: &Rect) -> (u16, u16) {
+        (rect.x + self.cursor_x, rect.y + self.cursor_y)
+    }
+
+    fn receive_key(&mut self, event: KeyEvent) -> bool {
+        match event.code {
+            KeyCode::Backspace => {
+                match self.text.pop() {
+                    None => {
+                        self.cursor_x = self.min_x;
+                        self.cursor_y = self.min_y;
+                    }
+                    Some(c) => {
+                        match c {
+                            '\n' => {
+                                self.cursor_y -= 1;
+                                self.cursor_x = self.min_x;
+
+                                // count from back until a newline is reached
+                                for c in self.text.chars().rev() {
+                                    if c == '\n' {
+                                        break;
+                                    }
+                                    self.cursor_x += 1;
+                                }
+                            }
+                            _ => {
+                                self.cursor_x -= 1;
+                            }
+                        }
+                    }
+                }
+            }
+            KeyCode::Delete => {
+                // ??
+            }
+            KeyCode::Enter => {
+                self.text.push('\n');
+                self.cursor_y += 1;
+                self.cursor_x = 1;
+            }
+            KeyCode::Char(c) => {
+                self.cursor_x += 1;
+                self.text.push(c);
+            }
+            _ => return false
+        }
+
+        true
+    }
+
+    fn set_active(&mut self) {
+        todo!()
+    }
+}
+
+type EditorFrame<'a> = Frame<'a, CrosstermBackend<Stdout>>;
 
 fn main() -> Result<(), io::Error> {
     enable_raw_mode()?;
@@ -20,16 +137,7 @@ fn main() -> Result<(), io::Error> {
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
-    // let mut input = String::new();
-    let mut text = String::new();
-
-    let (min_x, min_y) = (1, 1);
-    let (mut cursor_x, mut cursor_y) = (min_x, min_y);
-    // let mut workspace_size = 25;
-    // let mut input_size = 3;
-    // let mut input_active = false;
-    //
-    // let mut active_text = &mut text;
+    let mut edit_panel = TextEditPanel::new();
 
     loop {
         terminal.draw(|f| {
@@ -40,66 +148,18 @@ fn main() -> Result<(), io::Error> {
                 .constraints([Constraint::Length(size.width)])
                 .split(size);
 
-            let para_text = Text::from(text.as_str());
-            let para_block = Block::default().title("Block").borders(Borders::ALL);
-            let para = Paragraph::new(para_text)
-                .block(para_block)
-                .style(Style::default().fg(Color::White).bg(Color::Black))
-                .alignment(Alignment::Left)
-                .wrap(Wrap { trim: true });
-
-            let (start_x, start_y) = (layout[0].x, layout[0].y);
-
-            // + 1 from borders
-            f.set_cursor(start_x + cursor_x, start_y + cursor_y);
-
-            f.render_widget(para, layout[0]);
+            let (x, y) = edit_panel.get_cursor(&layout[0]);
+            f.set_cursor(x, y);
+            edit_panel.make_widget(f, layout[0]);
         })?;
 
         match read()? {
             Event::Key(event) => {
-                match event.code {
-                    KeyCode::Esc => break,
-                    KeyCode::Backspace => {
-                        match text.pop() {
-                            None => {
-                                cursor_x = min_x;
-                                cursor_y = min_y;
-                            }
-                            Some(c) => {
-                                match c {
-                                    '\n' => {
-                                        cursor_y -= 1;
-                                        cursor_x = min_x;
-
-                                        // count from back until a newline is reached
-                                        for c in text.chars().rev() {
-                                            if c == '\n' {
-                                                break;
-                                            }
-                                            cursor_x += 1;
-                                        }
-                                    }
-                                    _ => {
-                                        cursor_x -= 1;
-                                    }
-                                }
-                            }
-                        }
+                if !edit_panel.receive_key(event) {
+                    match event.code {
+                        KeyCode::Esc => break,
+                        _ => ()
                     }
-                    KeyCode::Delete => {
-                        // ??
-                    }
-                    KeyCode::Enter => {
-                        text.push('\n');
-                        cursor_y += 1;
-                        cursor_x = 1;
-                    }
-                    KeyCode::Char(c) => {
-                        cursor_x += 1;
-                        text.push(c);
-                    }
-                    _ => (), // execute!(terminal.backend_mut(), Print(format!("{:?}", event)))?
                 }
             }
             Event::Mouse(_event) => (), // println!("{:?}", event),
